3e4a35fcbe3ea83eda31b6bc0d8125c7250fa37b
jhovfries <jhovanariverab@gmail.com>
2024-06-10 18:26:14 -0600

    arrays 4,5,6

--- app.js
+++ app.js
@@ -35,13 +35,14 @@ let weekDays=['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', '
 console.log(weekDays);
 
 //Ejercicio-2-Arrays??
-const days=[
+let days=[
 'lunes',
 'martes',
 'miércoles',
 'jueves',
 'viernes'];
-console.log(days.toUpperCase());
+let cap= days.map(day => day.charAt(0).toUpperCase()+day.slice(1));
+console.log(cap);
 
 //Ejercicio-3-Arrays
 var num=arr(10);
@@ -51,3 +52,50 @@ var impar= arr.filter ((cont)=>cont%2!==0);
 console.log(par);
 console.log(impar);
 
+//Ejercicio-4-Arrays
+var arrnum=arrNumbers(10);
+let sumPar=0;
+let sumImpar=0;
+for(let i=0; i<arrnum.length; i++){
+    if(arrnum[i]%2===0) {
+        sumPar+=arrnum[i];
+    } else {
+        sumImpar+=arrnum[i]; 
+    }
+}
+console.log (sumPar);
+console.log (sumImpar);
+
+//Ejercicio-5-Arrays
+function repetidos(claseA, claseB){
+    let contentRep=[];
+    for(let i=0; i<claseA.length; i++){
+    let elemen=claseA[i];
+        if(claseB.includes(elemen)&& !contentRep.includes(elemen)){
+            contentRep.push(elemen);
+        }
+
+    }
+    console.log(contentRep);
+}
+//Ejercicio-6-Arrays
+let fam = [
+    { name: "Juan", edad: 34 },
+    { name: "Pepa", edad: 40 },
+    { name: "Ana", edad: 12 },
+    { name: "Luis", edad: 14 } ];
+
+    let mayor=fam[0];
+    let menor=fam[0];
+    
+
+for(let i=0; i<fam.length; i++){
+    if(fam[i].edad>mayor.edad){
+        mayor=fam[i];
+    }
+    if(fam[i].edad<menor.edad){
+        menor=fam[i];
+    }
+    console.log(`Miembro mayor: ${mayor.name} de ${mayor.edad} años.`);
+    console.log(`Miembro más joven: ${menor.name} de ${menor.edad} años.`);
+}
